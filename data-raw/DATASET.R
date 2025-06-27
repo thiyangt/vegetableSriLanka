dc6779d5f838c801f9387375125aed23afdd0ea9
Thisuri01 <thisuritj@gmail.com>
2025-06-27 12:08:19 +0530

    Cleaned dataset

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,12 +1,23 @@
 ## packages
-library(here)
-
+library(readxl)
+library(tidyr)
+library(dplyr)
 
 ## Read data
+data <- read_excel("C:/Users/thisu/Desktop/Final project/4. Final data/combined_data_new_cleaned.xlsx")
+
+colnames(data) <- c("Date", "Item",
+                    "Wholesale_Pettah", "Wholesale_Dambulla",
+                    "Retail_Pettah", "Retail_Dambulla")
+
+data <- data[-1, ]
 
 
-
-
+data <- data %>%
+  pivot_longer(cols = -c(Date, Item),
+               names_to = c("Type", "Market"),
+               names_sep = "_",
+               values_to = "Price")
 
 ## save dataset
 ## Save the dataset as vegetables.srilanka
